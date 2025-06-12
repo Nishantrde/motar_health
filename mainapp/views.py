79a0044efe45fb941a8fc06c98a260366740ee69
Nishant <nishant2024rde@gmail.com>
2025-06-12 07:07:39 +0530

    Initial Commit

--- mainapp/views.py
+++ mainapp/views.py
@@ -6,19 +6,19 @@ from django.views import View
 from .rto import fetch_carinfo
 
 class Index(View):
-    async def get(self, request):
-        carinfo = await sync_to_async(fetch_carinfo)("JH10BK5078")
+     def get(self, request):
+        carinfo = fetch_carinfo("JH10BK5078")
         print(carinfo)
         return render(request, "index.html")
 
 class RC_info(View):
-    async def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         # 1) Read and parse the JSON body (no await)
         data = json.loads(request.body)
         text = data.get('text', '')
         
         # 2) Call the blocking fetch_carinfo in a threadpool
-        carinfo = await sync_to_async(fetch_carinfo)(text)
+        carinfo = fetch_carinfo(text)
         print(carinfo)
         # 3) Return the result
         return JsonResponse({
